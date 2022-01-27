1138cc87320a68c34e6d8be0d59a951b94871544
karmz-crypto <vivek.303826@gmail.com>
2022-01-27 18:24:19 +0530

    sales.js page

--- static/js/addSalesPage.js
+++ static/js/addSalesPage.js
@@ -1,5 +1,6 @@
 window.onload = ()=>{
     console.log('sales connected');
+   // genericBtn();
 };
 
 var client = {
@@ -54,6 +55,144 @@ var product ={
 
 };
 
+var productModal ={
+    productId:'',
+    itemPurity:0,
+    saleWastage:0,
+    saleLabourMax:0,
+    saleLabourMin:0,
+    itemName:'',
+    inStock:0,
+    netFineSilver:0,
+    netSaleCash:0,
+    itemWeight:0,
+    modalId:'',
+    itemWeight:0,
+    calInStock:0,
+    /*deleteModalBtn:function(){
+        
+    },
+    createModalBtn:function(){},*/
+    setModalDataVariables:function(data){
+        console.log(data);
+        this.itemPurity = data.itemPurity;
+        this.inStock = data.inStockQuantity;
+        this.itemName = data.itemName;
+        this.saleLabourMax=data.saleLabourMax;
+        this.saleLabourMin = data.saleLabourMin;
+        this.saleWastage = data.purchasePurity+data.salePurity;
+        this.insertDataInModal();
+    },
+    insertDataInModal:function(){
+        //console.log(this.modalId);
+        document.querySelector('.modal-title').innerHTML = this.itemName;
+        document.querySelector('.itemPurity').innerHTML = this.itemPurity;
+        document.querySelector('.inStock').innerHTML = this.inStock;
+        document.querySelector('#saleWastage').value = this.saleWastage;
+        document.querySelector('.saleWastage_display').innerHTML = document.querySelector('#saleWastage').value;
+        document.querySelector('#saleLabour').value = this.saleLabourMax;
+        document.querySelector('.saleLabour_display').innerHTML= document.querySelector('#saleLabour').value;
+        this.openProductModal();
+        
+    },
+    fetchProductData:function(){ 
+        let url = `/apiPageRouter/getSelectedProduct/${this.productId}`;
+        fetchData(url,'product');
+    },
+    isProductUnique:function(event,modalId){ //console.log('here');
+        this.productId = event.target.value;
+        this.modalId = modalId;
+        document.querySelector('.salesTable').querySelectorAll('select').forEach(e=>{  //console.log('here2')
+            //select is only for the product id
+            if(e.value===this.productId){
+                //open a warning modal & return
+            }
+            
+        });this.fetchProductData();
+    },
+    openProductModal:function(){
+        //this.inserDataInModal();
+        this.initializeModal();
+        var btn = genericBtn()
+        btn.removeAttribute('data-bs-target');
+        btn.setAttribute('data-bs-target',this.modalId);
+        //console.log(btn);
+        btn.click();
+    },
+    initializeModal:function(){ console.log(this.netFineSilver);
+        document.querySelector('#itemWeight').value = 0;
+        document.querySelector('.calInStock').innerHTML = this.calInStock;
+        document.querySelector('#netFineSilverModal-input').value = this.netFineSilver;
+        document.querySelector('.netFineSilverModal').innerHTML= document.querySelector('#netFineSilverModal-input').value ;
+        document.querySelector('#netCashModal-input').value = this.netSaleCash;
+        document.querySelector('.netCashModal').innerHTML = document.querySelector('#netCashModal-input').value;
+    },
+    editWastage:function(){},
+    editLabour:function(){},
+    calculateNetFineSilver:function(){
+        document.querySelector('#netFineSilverModal-input').value = parseFloat(this.itemWeight)*(parseFloat(this.itemPurity)+parseFloat(this.saleWastage))/100;
+        document.querySelector('.netFineSilverModal').innerHTML = document.querySelector('#netFineSilverModal-input').value;
+    },
+    calculateNetSaleCash:function(){},
+    calculateStock:function(event){
+        this.itemWeight = event.target.value;
+        document.querySelector('.calInStock').innerHTML = parseFloat(this.inStock)-parseFloat(this.itemWeight);
+        this.calculateNetFineSilver();
+    },
+    transferModalData:function(){}
+
+};
+
+var warningModal = {
+    warningMsg:'',
+    setWarningMsg:function(warningMsg){
+        this.warningMsg = warningMsg;
+    }
+}
+
+function genericBtn(){
+    var btn = {
+        el:'button',
+        htmlClass:['d-none','genericBtn'],
+        attr:{'type':"button", 'class':"btn btn-primary", 'data-bs-toggle':"modal", 'data-bs-target':'none'}
+    };
+    var btnEl =createElement(btn);
+    appendElement(btnEl,document.querySelector('.modalBtn'));
+    return btnEl;
+}
+
+function createElement(arg){
+    var element = document.createElement(arg.el);
+    addClassList(element,arg);
+    setAttribute(element,arg);
+    textNode(element,arg);
+    return element;
+}
+
+
+function addClassList(element,arg){
+    arg.htmlClass.forEach(e=> {
+        element.classList.add(e)
+    });
+    
+}
+
+function setAttribute(element, arg) {
+    for(var key in arg.attr) {
+      element.setAttribute(key, arg.attr[key]);
+    }
+    
+  }
+
+  function textNode(element,arg){
+      element.appendChild(document.createTextNode(arg.text));
+  }
+
+  function appendElement(element,parent){
+      parent.appendChild(element);
+     // console.log(element);
+  }
+
 
 function fetchData(url,identifier){
     fetch(url).then(
@@ -67,7 +206,7 @@ function fetchData(url,identifier){
             response.json().then(function(data) {
              // console.log(data); -- it contains the data from server and now can be used 
              if(identifier==='product'){
-                insertDataIntoModal(data); //this func inserts data into modal from fetched data.......  
+                productModal.setModalDataVariables(data); //this func inserts data into modal from fetched data.......  
              }
              if(identifier==='client'){
                 client.setClientData(data);
